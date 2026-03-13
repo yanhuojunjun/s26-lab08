50b125c2dd6c622c46a5ad621faed0fa438c8698
yanhuojunjun <yanhuojunjun@gmail.com>
2026-03-13 16:15:42 -0400

    Finish lab08

--- src/main/java/edu/cmu/cs/cs214/rec08/map/SimpleHashMap.java
+++ src/main/java/edu/cmu/cs/cs214/rec08/map/SimpleHashMap.java
@@ -56,16 +56,17 @@ public class SimpleHashMap<K, V> {
             throw new NullPointerException("Key can't be null.");
 
         List<Entry<K,V>> bucket = table.get(hash(key));
-        for (Entry<K, V> e : bucket) {
-            if (e.key.equals(key)) {
-                V result = e.value;
-                e.value = value;
-                return result;
+        synchronized (bucket) {
+            for (Entry<K, V> e : bucket) {
+                if (e.key.equals(key)) {
+                    V result = e.value;
+                    e.value = value;
+                    return result;
+                }
             }
-        }
-
-        bucket.add(new Entry<>(key, value));
-        return null;
+            bucket.add(new Entry<>(key, value));
+            return null;
+        }        
     }
 
     /**
@@ -76,12 +77,14 @@ public class SimpleHashMap<K, V> {
      */
     public V get(K key) {
         List<Entry<K,V>> bucket = table.get(hash(key));
-        for (Entry<K, V> e : bucket) {
-            if (e.key.equals(key)) {
-                return e.value;
+        synchronized (bucket) {
+            for (Entry<K, V> e : bucket) {
+                if (e.key.equals(key)) {
+                    return e.value;
+                }
             }
+            return null;
         }
-        return null;
     }
 
     /**
